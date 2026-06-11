924cc6d23b6b5dc83a2a7b93859cda3737897f35
DevinPatel72 <devin.patel72@gmail.com>
2026-06-11 15:37:14 -0500

    Undid schema problem

--- src/parsers/parser_tools/toolbox.py
+++ src/parsers/parser_tools/toolbox.py
@@ -281,7 +281,7 @@ def check_CWE_category(cwe, count=0):
         return cwe, count
 
 def export_config(inputs, outfile, control_flags):
-    out_dict = {"$schema": "../schemas/user_inputs.schema.json",
+    out_dict = {"$schema": "schemas/user_inputs.schema.json",
                 'project_name': parsers.PROJ_NAME,
                 'project_version': parsers.PROJ_VERSION,
                 'main': inputs,
